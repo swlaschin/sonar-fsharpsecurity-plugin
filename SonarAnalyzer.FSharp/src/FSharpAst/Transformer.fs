abccf7c4613b1ca74c0e0eeedba4a0e169c98706
swlaschin <swlaschin@gmail.com>
2019-09-04 11:58:36 +0100

    comment out BasicPatterns.TraitCall warning

--- SonarAnalyzer.FSharp/src/FSharpAst/Transformer.fs
+++ SonarAnalyzer.FSharp/src/FSharpAst/Transformer.fs
@@ -978,7 +978,7 @@ type FileTransformer(config:TransformerConfig) =
             /// Matches expressions for an unresolved call to a trait
             | BasicPatterns.TraitCall(tys:FSharpType list, name:string, memberFlags:FSharp.Compiler.Ast.MemberFlags, tys2:FSharpType list, argTypes:FSharpType list, argExprs:FSharpExpr list) ->
                 let msg = "BasicPatterns.TraitCall: not implemented"
-                warn msg
+                //warn msg
                 Tast.UnknownExpression msg
 
             | pattern ->
